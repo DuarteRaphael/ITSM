a4ecd3779643d24b8c8bf92f02d4a55b5c484613
Raphael Duarte <raphael-duarte@outlook.com>
2024-06-15 08:23:14 -0300

    Adicionar arquivos de projeto.

---
+++ ITSM.Services.Api/Controllers/ClientesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITSM.Services.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Cadastrar()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Atualizar()
+        {
+            return Ok();
+        }
+        [HttpDelete("id")]
+        public IActionResult Excluir(Guid id)
+        {
+            return Ok();
+        }
+        [HttpGet("page/limit")]
+        public IActionResult BuscarTodos(int page, int limit)
+        {
+            return Ok();
+        }
+        [HttpGet("id")]
+        public IActionResult BuscarPorId(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
